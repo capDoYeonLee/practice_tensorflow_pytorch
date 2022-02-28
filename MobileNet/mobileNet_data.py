794ce04e5e9f2e84b89c1b92e35c8dda4686dae7
capDoYeonLee <ghtyu21@naver.com>
2022-02-28 17:31:09 +0900

    MobileNet

---
+++ MobileNet/mobileNet_data.py
@@ -0,0 +1,49 @@
+import torch
+from torch.utils.data import Dataset
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+import os 
+import pandas as pd
+from torchvision.io import read_image
+from torch.utils.data import DataLoader
+
+class CustomImageDataset(Dataset):
+    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
+        self.img_labels = pd.read_csv(annotations_file, names=['file_name', 'label'])
+        self.img_dir = img_dir
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        return len(self.img_labels)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+        image = read_image(img_path)
+        label = self.img_labels.iloc[idx, 1]
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            label = self.target_transform(label)
+        return image, label
+
+training_data = datasets.FashionMNIST(
+    root = 'data',
+    download = True,
+    transform = ToTensor()
+)
+
+test_data = datasets.FashionMNIST(
+    root = 'data',
+    train=False,
+    download = True,
+    transform = ToTensor()
+)
+
+
+
+train_dataloader = DataLoader(training_data, batch_size=64, shuffle=True)
+test_dataloader = DataLoader(test_data, batch_size=64, shuffle=True)
+
+
+print(train_dataloader)
