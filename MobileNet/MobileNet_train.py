a22f56162cea0647852d50276fc9eb21e84ef31f
capDoYeonLee <ghtyu21@naver.com>
2022-03-05 12:54:21 +0900

    ing

--- MobileNet/MobileNet_train.py
+++ MobileNet/MobileNet_train.py
@@ -2,6 +2,12 @@ from model import *
 from pascal_voc import *
 import tensorflow as tf
 
+train_x = img
+
+
+
+
+
 base_learning_rate = 0.0001
 model.compile(optimizer=tf.keras.optimizers.Adam(lr=base_learning_rate),
               loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
@@ -13,7 +19,7 @@ model = mobilenet(img.shape , n_classes)
 model.summary()
 
 initial_epochs = 10
-history = model.fit(img,
+history = model.fit(#미정,
                     epochs=initial_epochs,
                     # validation_data=validation_dataset
                     )
