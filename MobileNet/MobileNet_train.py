97c0cd6194cd266c42dcde76073fe7b4887f8f7f
capDoYeonLee <ghtyu21@naver.com>
2022-03-06 15:58:16 +0900

    ing

--- MobileNet/MobileNet_train.py
+++ MobileNet/MobileNet_train.py
@@ -2,26 +2,23 @@ from model import *
 from pascal_voc import *
 import tensorflow as tf
 
-train_x = img
 
 
 
 
 
 base_learning_rate = 0.0001
-model.compile(optimizer=tf.keras.optimizers.Adam(lr=base_learning_rate),
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=base_learning_rate),
               loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
 
 
-model = mobilenet(img.shape , n_classes)
-model.summary()
-
 initial_epochs = 10
-history = model.fit(#미정,
+history = model.fit(x=train_x, y=train_y,
                     epochs=initial_epochs,
                     # validation_data=validation_dataset
+                    batch_size=32
                     )
                     
 
