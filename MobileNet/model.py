ab41f3f04d4d00aa4414ab06d095b2a54211a973
capDoYeonLee <ghtyu21@naver.com>
2022-03-09 17:10:55 +0900

    ing

--- MobileNet/model.py
+++ MobileNet/model.py
@@ -1,3 +1,4 @@
+import tensorflow as tf
 import keras
 import keras.backend as K
 from keras.models import Model
@@ -41,25 +42,10 @@ def mobilenetv1(x,alph = 1): # 224 224 3
     return x
 
 
-filename = 'checkpoint-epoch-{}-batch-{}-trial-001.h5'.format(30, 128)
-checkpoint = ModelCheckpoint(filename,monitor='val_loss', verbose=1,save_best_only=True, mode='auto')
-earlystopping = EarlyStopping(monitor='val_loss',patience=10)
-reduceLR = ReduceLROnPlateau( monitor='val_loss',factor=0.5,patience=3,)
 
-
-inputs = Input(shape = (32,32,3),dtype=np.float32)
-x = mobilenetv1(inputs)
-x = tf.keras.layers.GlobalAveragePooling2D()(x)
-outputs = Dense(10, activation='softmax')(x)
-model = tf.keras.models.Model(inputs,outputs)
-nadam = tf.keras.optimizers.Nadam(lr=0.01)
-model.compile(optimizer=nadam,loss='categorical_crossentropy',metrics=['accuracy'])
-
-model.fit(x_train,y_train,batch_size=128, epochs=30,validation_split=0.1,callbacks=[reduceLR,checkpoint,earlystopping])
-
-
-
-
+model = tf.keras.Sequentail(
+    
+)
 
 
 
