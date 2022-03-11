1902e2efb9d443429e9f23fad9046f678fa19529
capDoYeonLee <ghtyu21@naver.com>
2022-03-11 12:17:06 +0900

    ing

--- MobileNet_Pytorch/model.py
+++ MobileNet_Pytorch/model.py
@@ -1,1 +1,103 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchsummary import summary
+from torch import optim
+
+
+class Depthwise(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1):
+        super().init()
+        
+        self.depthwise = nn.Sequential(
+            nn.Conv2d(in_channels, in_channels, 3, stride=stride, padding=1, groups=in_channels, bias=False), # 3 is kernel size
+            nn.BatchNorm2d(in_channels),
+            nn.ReLU(),
+        )
+        self.pointwise = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 1, stride=stride, padding=0, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(),
+        )
+        
+    def forward(self, x):
+        x = self.depthwise(x)
+        x = self.pointwise(x)
+        
+        return x
+    
+    
+    
+class BasicConv2d(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, **kwargs):
+        super().__init__()
+        
+        self.conv == nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size, **kwargs),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(),
+        )
+    
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+    
+
+
+
+class MobileNet(nn.module):
+    def __init__(self, width_multiplier, num_classes=10, init_weights=True):
+        super().__init__()
+        self.init_weights = init_weights
+        alpha = width_multiplier
+        
+        
+        self.conv1 = BasicConv2d(3, int(32*alpha), 3, stride=2, padding=1)     # in_ch=3, out_ch=32, kernel_size=3
+        self.conv2 = Depthwise(int(32*alpha), int(64*alpha), stride=1)
+        
+        self.conv3 = nn.Sequential(
+            Depthwise(int(64*alpha), int(128*alpha), stride=2),
+            Depthwise(int(128*alpha), int(128*alpha), stride=1)
+        )
+        
+        self.conv4 = nn.Sequential(
+            Depthwise(int(128*alpha), int(256*alpha), stride=2),
+            Depthwise(int(256*alpha), int(256*alpha), stride=1)
+        )
+        
+        self.conv5 = nn.Sequential(
+            Depthwise(int(256*alpha), int(512*alpha), stride=2),
+            Depthwise(int(512*alpha), int(512*alpha), stride=1),
+            Depthwise(int(512*alpha), int(512*alpha), stride=1),
+            Depthwise(int(512*alpha), int(512*alpha), stride=1),
+            Depthwise(int(512*alpha), int(512*alpha), stride=1),
+            Depthwise(int(512*alpha), int(512*alpha), stride=1),
+        )
+        
+        self.conv6 = nn.Sequential(
+            Depthwise(int(512*alpha), int(1024*alpha), stride=2)
+        )
+        
+        self.conv7 = nn.Sequential(
+            Depthwise(int(1024*alpha), int(1024*alpha), stride=2)
+        )
+        
+        self.avg_pool = nn.AdaptiveAvgPool2d((1,1))
+        self.linear = nn.Linear(int(1024*alpha), num_classes)
+
+        # weights initialization
+        if self.init_weights:
+            self._initialize_weights()
+    
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.conv5(x)
+        x = self.conv6(x)
+        x = self.conv7(x)
+        x = self.avg_pool(x)
+        x = x.view(x.size(0), -1)      # 뭐지?
+        x = self.linear(x)
+        return x
